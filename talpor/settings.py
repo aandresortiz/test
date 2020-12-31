ebdcc8f97324dd74d4d29ea84ae1c67738c4658e
Angel Ortiz <aandresortiz@gmail.com>
2020-12-31 21:11:43 +0000

    added gunicorn with docker and compose support

--- talpor/settings.py
+++ talpor/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "-$m^kf!8q@^xm60w0$zrve!w4h043asm&wdjznzfa@bbozum#1"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 0) != 0
 
 ALLOWED_HOSTS = []
 

0e26d75e4724551a1f6465c33da955e0abfe798e
Angel Ortiz <aandresortiz@gmail.com>
2020-12-31 18:54:08 +0000

    added User, Order and OrderItem models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    email = models.CharField(max_length=60)
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+    date = models.DateField(auto_now=True)
+    status = models.CharField(max_length=15)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    description = models.CharField(max_length=400)
+    quantity = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
